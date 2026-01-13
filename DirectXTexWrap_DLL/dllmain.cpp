44490b029d6afd7d876cec36056c963ce8e9d225
charles7668 <charles76680119@gmail.com>
2026-01-13 21:06:21 +0800

    feat: Add GetMetadataFromDDSMemory , LoadFromDDSMemory wrap function

--- DirectXTexWrap_DLL/dllmain.cpp
+++ DirectXTexWrap_DLL/dllmain.cpp
@@ -1,19 +1,46 @@
 ﻿// dllmain.cpp : 定義 DLL 應用程式的進入點。
 #include "pch.h"
+#include "DirectXTex.h"
 
-BOOL APIENTRY DllMain( HMODULE hModule,
-                       DWORD  ul_reason_for_call,
-                       LPVOID lpReserved
-                     )
+BOOL APIENTRY DllMain(HMODULE hModule,
+                      DWORD ul_reason_for_call,
+                      LPVOID lpReserved
+)
 {
-    switch (ul_reason_for_call)
-    {
-    case DLL_PROCESS_ATTACH:
-    case DLL_THREAD_ATTACH:
-    case DLL_THREAD_DETACH:
-    case DLL_PROCESS_DETACH:
-        break;
-    }
-    return TRUE;
+	switch (ul_reason_for_call)
+	{
+	case DLL_PROCESS_ATTACH:
+		{
+			HRESULT hr = CoInitializeEx(nullptr, COINIT_MULTITHREADED);
+			if (FAILED(hr))
+				return FALSE;
+		}
+		break;
+	case DLL_THREAD_ATTACH:
+	case DLL_THREAD_DETACH:
+	case DLL_PROCESS_DETACH:
+		break;
+	}
+	return TRUE;
 }
 
+extern "C" __declspec(dllexport) _Use_decl_annotations_
+HRESULT GetMetadataFromDDSMemory(
+	const uint8_t* pSource,
+	size_t size,
+	DirectX::DDS_FLAGS flags,
+	DirectX::TexMetadata& metadata) noexcept
+{
+	return DirectX::GetMetadataFromDDSMemoryEx(pSource, size, flags, metadata, nullptr);
+}
+
+extern "C" __declspec(dllexport) _Use_decl_annotations_
+HRESULT LoadFromDDSMemory(
+	const uint8_t* pSource,
+	size_t size,
+	DirectX::DDS_FLAGS flags,
+	DirectX::TexMetadata* metadata,
+	DirectX::ScratchImage& image) noexcept
+{
+	return DirectX::LoadFromDDSMemoryEx(pSource, size, flags, metadata, nullptr, image);
+}
